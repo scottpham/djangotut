a95933ea823e3d9b6cebf23d6dac09675226fd27
Scott Pham <scott.pham85@gmail.com>
2016-05-12 14:59:42 -0700

    form

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
 # 127.0.0.1:8000
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
